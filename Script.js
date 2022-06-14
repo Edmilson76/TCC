4e5e713dc803fb0caefce843dfd1df0ac23f0d32
Edmilson76 <brito.edmilson@terra.com.br>
2022-06-14 18:52:19 -0300

    Ajustes no botao carregar!

--- Script.js
+++ Script.js
@@ -3,38 +3,40 @@ const tabela = document.querySelector("#tabela-iphone");
 
 botao.addEventListener('click', function() {
     carregarDados();
+
 })
 
 function carregarDados() {
-fetch('http//127.0.0.1:5000/todos')
+    fetch('http://127.0.0.1:5000/todos')
     .then(function(resposta) {
-        return resposta.json()
-    }).then(function(ListaAluno) {
-        popularTabela(ListaAluno);
+        return resposta.json();
+    })
+    .then(function(ListaIphone) {
+      popularTabela(ListaIphone);
+       
     })
 }
-function popularTabela(listaAluno){
-    const tamanhoLista = listaAluno.length;
+function popularTabela(listaIphone){
+    const tamanhoLista = listaIphone.length;
     
     for(let index = 0; index < tamanhoLista; index++) {
-        console.log(listaAluno[index]);
-        const linha =document.createElement('tr');
+        const linha = document.createElement('tr');
+
         const id = document.createElement('td'); 
         const Modelo = document.createElement('td'); 
         const Capacidade = document.createElement('td'); 
         const preco = document.createElement('td'); 
-        id.innerHTML = listaAluno[index][0];
-        Modelo.innerHTML = listaAluno[index][1];
-        Capacidade.innerHTML = listaAluno[index][2];
-        preco.innerHTML = listaAluno[index][3];
+
+        id.innerHTML = listaIphone[index][0];
+        Modelo.innerHTML = listaIphone[index][1];
+        Capacidade.innerHTML = listaIphone[index][2];
+        preco.innerHTML = listaIphone[index][3];
         
         linha.appendChild(id); 
         linha.appendChild(Modelo);
         linha.appendChild(Capacidade);
-        linha.appendChild(preco)
-
-        tabela. appendChild(linha);
-     
+        linha.appendChild(preco);    
+        tabela.appendChild(linha);
     }
     
 }
